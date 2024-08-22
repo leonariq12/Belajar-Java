0f57f862143ad02d9db6d0bf05a76173db367cd8
leonariq12 <113584474+leonariq12@users.noreply.github.com>
2024-08-22 14:38:16 +0700

    Add files via upload

---
+++ Modul4.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package modul4;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Modul4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        tugas6 modul=new tugas6();
+        modul.setVisible(true);
+    }
+
+    
+    
+    
+}
